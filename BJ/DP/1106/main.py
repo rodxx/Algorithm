7408aa8c6f23450424e318a56f74fde60d229522
Lieutenant-K <kts4047@gmail.com>
2021-11-25 04:01:04 +0900

    BJ 1106
    백준 1106번 : 호텔

--- BJ/DP/1106/main.py
+++ BJ/DP/1106/main.py
@@ -18,24 +18,3 @@ c, n = map(int, input().split())
 city = [list(map(int, input().split())) for _ in range(n)]
 d = [-1]*(c+1)
 print(sol(c))
-
-'''
-import sys
-input = sys.stdin.readline
-
-def sol(pay, customer):
-    if customer <= 0:
-        return pay
-    #print(customer)
-    m = 100000
-    for i in range(n):
-        m = min(m, sol(pay + city[i][0], customer - city[i][1]))
-
-    return m
-
-c, n = map(int, input().split())
-city = [list(map(int, input().split())) for _ in range(n)]
-d = [[0]*n for _ in range(n)]
-#print(city)
-print(sol(0, c))
-'''
