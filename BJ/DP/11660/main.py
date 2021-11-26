81148d053413d2eef0e4e422c5a4cf4b862f840a
Lieutenant-K <kts4047@gmail.com>
2021-11-27 02:52:10 +0900

    BJ 11660
    백준 11660번 : 구간 합 구하기 5

--- BJ/DP/11660/main.py
+++ BJ/DP/11660/main.py
@@ -3,28 +3,14 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 s = [list(map(int, input().split())) for _ in range(n)]
-d = [[0]*n for _ in range(n)]
+d = [[0]*(n+1) for _ in range(n+1)]
 
-for x in range(n):
-    for y in range(n):
-        if x-1 >= 0:
-            d[x][y] += d[x-1][y]
-        if y-1 >= 0:
-            d[x][y] += d[x][y-1]
-        if x-1 >= 0 and y-1 >= 0:
-            d[x][y] -= d[x-1][y-1]
-        d[x][y] += s[x][y]
+for x in range(1, n+1):
+    for y in range(1, n+1):
+        d[x][y] = s[x-1][y-1] + d[x-1][y] + d[x][y-1] - d[x-1][y-1]
 
 for _ in range(m):
     i = list(map(int, input().split()))
-    x1, y1, x2, y2 = i[0]-1, i[1]-1, i[2]-1, i[3]-1
-    r = d[x2][y2]
-
-    if y1-1 >= 0:
-        r -= d[x2][y1-1]
-    if x1-1 >= 0:
-        r -= d[x1-1][y2]
-    if x1-1 >= 0 and y1-1 >= 0:
-        r += d[x1-1][y1-1]
-
+    x1, y1, x2, y2 = i[0], i[1], i[2], i[3]
+    r = d[x2][y2] - d[x2][y1-1] - d[x1-1][y2] + d[x1-1][y1-1]
     print(r)
