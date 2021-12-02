118a9d14c0f4fd75c3c018bd3697a370722a9b36
Lieutenant-K <kts4047@gmail.com>
2021-12-02 16:56:32 +0900

    BJ 11722
    백준 11722번 : 가장 긴 감소하는 부분 수열

---
+++ BJ/DP/11722/main.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+s = list(map(int, input().split()))
+d = [1]*n
+for i in range(1, n):
+    for j in range(i):
+        if s[i] < s[j]:
+            d[i] = max(d[i], d[j]+1)
+print(max(d))
