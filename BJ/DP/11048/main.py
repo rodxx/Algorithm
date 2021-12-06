b71b1862360801f8b8132b730d7a478b90d442b2
Lieutenant-K <kts4047@gmail.com>
2021-12-06 15:56:07 +0900

    BJ 11048
    백준 11048번 : 이동하기

---
+++ BJ/DP/11048/main.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+d = [[0]*(m+1)] + [([0] + list(map(int, input().split()))) for _ in range(n)]
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        d[i][j] += max(d[i-1][j], d[i][j-1], d[i-1][j-1])
+print(d[n][m])
